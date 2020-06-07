bfa6994735854752d517e878485a678cea4137de
Mohamad Alshaaer <62287575+Mohamadalshaaer@users.noreply.github.com>
2020-06-07 16:53:55 +0200

    Update Functional-2.java

--- Functional-2.java
+++ Functional-2.java
@@ -12,7 +12,7 @@ public List<Integer> no9(List<Integer> nums) {
 
 //Given a list of integers, return a list of those numbers, omitting any that are between 13 and 19 inclusive.
 public List<Integer> noTeen(List<Integer> nums) {
-  nums.removeIf(n -> n >= 13 & n <= 19);
+  nums.removeIf(n -> n >= 13 && n <= 19);
   return nums;
 }
 
